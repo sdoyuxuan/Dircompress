e2b86d637b75664b4ff47da3ce375022c0238220
sdoyuxuan <513754057@qq.com>
2017-10-22 17:37:01 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "compress.h"
+
+int main(int argc , char*argv[])
+{
+     Filecompress object;
+ //  object.compress(argv[1]);
+     object.uncompress(argv[1]);
+ //  object.FindFile(argv[1]);
+ //  object.TestVector();
+   return 0;
+}
